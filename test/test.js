bb6e6ce8ef7b9dac491de042b05c6bdc54930b56
Vladimir Agafonkin <agafonkin@gmail.com>
2019-03-23 19:40:22 +0200

    use tape for testing

--- test/test.js
+++ test/test.js
@@ -3,34 +3,26 @@
 const rollup = require('rollup');
 const rollupUnassert = require('../dist/rollup-plugin-unassert.cjs.js');
 const fs = require('fs');
+const test = require('tape').test;
 
-// This is the example from https://github.com/unassert-js/unassert#example
-
-rollup.rollup({
-    input: 'test/original.js',
-    plugins: [
-        rollupUnassert()
-    ]
-}).then((bundle) => {
-    // Generate bundle + sourcemap
-    bundle.generate({
-        format: 'es',
-        sourcemap: 'inline'
-    }).then((result) => {
-        const code = result.output[0].code;
-        const expected = fs.readFileSync('test/expected.js').toString();
-
-        if (expected === code) {
-            console.log('rollup-plugin-unassert unit test passed');
-            process.exit(0);
-        } else {
-            console.log('rollup-plugin-unassert unit test failed');
-            console.log('Generated code: \n', code);
-            console.log('Expected code: \n', expected);
-            process.exit(-1);
-        }
+test('removes assert code', (t) => {
+    // This is the example from https://github.com/unassert-js/unassert#example
+    rollup.rollup({
+        input: 'test/original.js',
+        plugins: [
+            rollupUnassert()
+        ]
+    }).then((bundle) => {
+        bundle.generate({
+            format: 'es',
+            sourcemap: 'inline'
+        }).then((result) => {
+            const expected = fs.readFileSync('test/expected.js', 'utf8');
+            t.equal(result.output[0].code, expected);
+            t.end();
+        });
+    }).catch((e) => {
+        console.error(e);
+        process.exit(-1);
     });
-}).catch((e) => {
-    console.error(e);
-    process.exit(-1);
 });
